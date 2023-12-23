c36fab618a19b96fc248f671ff8a653bd0020367
jpb-personal <stellachelsey@gmail.com>
2023-12-23 11:19:40 +0100

    Attempts to fix CORS issues

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ const connectDB = async () => {
     }
 }
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send({text: 'test'});
 });
