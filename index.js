3aed98c76c1ade745e97ef1db4e38104f7e2a2e1
jpb-personal <stellachelsey@gmail.com>
2023-12-25 18:32:41 +0100

    Cleans up code

--- index.js
+++ index.js
@@ -1,158 +1,187 @@
+
+// -----
+// SETUP
+// -----
+
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
-
 const mongoose = require('mongoose');
-const Message = require('./models/message.js')
 const TelegramBot = require('node-telegram-bot-api');
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
+const helpers = require('./helpers.js')
 
-const app  = express();
-const PORT = process.env.PORT || 3000;
 
+// Initailize app
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.options('/add-message', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'app://obsidian.md');
-    res.setHeader('Access-Control-Allow-Methods', 'POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.status(200).end();
-});
-
+// Connect to database
 mongoose.set('strictQuery', false);
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB Connected: ' + conn.connection.host)
+        console.log('MongoDB | MongoDB Connected: ' + conn.connection.host)
     } catch (error) {
-        console.log(error);
+        console.error("MongoDB | " + error);
         process.exit(1);
     }
 }
+const PORT = process.env.PORT || 3000;
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log('App | Listening on port ' + PORT);
+    })
+})
+
+// Load database models
+const Message = require('./models/message.js');
+
+// Initailize telegram bot
+const bot = new TelegramBot(process.env.BOT_TOKEN);
 
 
 
-app.get('/', (req, res) => {
-    res.send({text: 'test'});
-});
+// -------------
+// CONFIGURATION
+// -------------
 
 
-app.get('/messages', async (req,res) => {
-    const message = await Message.find();
+// PRE: -
+// POST: returns all messages in database
+app.get('/messages', async (_req,res) => {
+    try {
+        // get all messages
+        const messages = await Message.find();
 
-    if (message) {
-        console.log("Messages found");
-        res.json(message);
-    } else {
-        res.send("Something went wrong.");
+        // return messages if any were found
+        if(messages) {
+            console.log("/messages | Messages found");
+            res.status(201).json({ message: "/messages | Messages found", messages: messages });
+        } else {
+            console.error("/messages | Messages could not be retrieved:", messages);
+            res.status(500).json({ error: "/messages | Messages could not be retrieved" });
+        }
+    } catch (error) {
+        console.error("/messages |", error);
+        res.status(500).json({ error: "/messages | Internal Server Error: " + error });
     }
 });
 
 
+// PRE: -
+// POST: will clear the entire database
+app.put('/clear', async (_req, res) => {
+    try {
+        await Message.deleteMany({});
+
+        console.log("/clear | Cleared all messages");
+        res.status(201).json({ message: "/clear | Cleared all messages"});
+    } catch(error){
+        console.error("/clear | " + error);
+        res.status(500).json({ error: "/clear | Internal Server Error: " + error });
+    }
+})
+
+
+// PRE: body must include text, scheduled_time, type
+//      scheduled_time must be a js date
+//      type must be either 'event', 'task', or 'todo'
+// POST: adds corresponding message to database
 app.post('/add-message', async (req, res) => {
     try {
+        // extract input and confirm it is complete
         const { text, scheduled_time, type } = req.body;
-
         if (!text || !scheduled_time || !type) {
-            return res.status(400).json({ error: 'Text, scheduled_time, and type are required' });
+            console.error("/add-messages | Body incomplete:", req.body);
+            res.status(400).json({ error: "/add-messages | Body incomplete: " + req.body });
+            return;
         }
 
+        // save message
         const newMessage = new Message({
             text,
             scheduled_time: new Date(scheduled_time),
             type,
         });
-
         await newMessage.save().then(msg => {
             const id = msg.id;
-            res.status(201).json({ message: 'Message added to the database successfully', id });
+            res.status(201).json({ message: "Message successfully added to the database", id: id });
         });
-
     } catch (error) {
-        console.error("ERROR | " + error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        console.error("/add-message |", error);
+        res.status(500).json({ error: "/add-message | Internal Server Error: " + error });
     }
 });
 
 
+// PRE: body must contain the message's id
+//      id was provided by the API when the message was added to the database
+// POST: removes the corresponding message from the database
 app.put('/delete-message', async (req, res) => {
     try {
+        // extract input and confirm it is complete
         const { id } = req.body;
-
         if (!id) {
-            return res.status(400).json({ error: 'Id is required' });
+            console.error("/delete-message | Body incomplete:", req.body);
+            return res.status(400).json({ error: "/delete-message | Body incomplete: " + req.body });
         }
 
+        // delete message
         await Message.findOneAndDelete({ _id: id }).then(() => {
-            res.status(201).json({ message: 'Message successfully deleted'});
+            console.log("/delete-message | Message", id, "successfully deleted");
+            res.status(201).json({ message: "/delete-message | Message " + id + " successfully deleted" });
         });
 
     } catch (error) {
-        console.error("ERROR | " + error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        console.error("/delete-message |", error);
+        res.status(500).json({ error: "/delete-message | Internal Server Error: " + error });
     }
 })
 
 
+// PRE: body must contain id, text, scheduled_time, type
+//      id was provided by the API when the message was added to the database
+//      scheduled_time must be a js date
+//      type must be either 'event', 'task', or 'todo'
+// POST: will alter the corresponding message's text, scheduled_time, and type
 app.put('/modify-message', async (req,res) => {
     try {
+        // extract input and confirm it is complete
         const { id, text, scheduled_time, type } = req.body;
-
         if (!id || !text || !scheduled_time || !type) {
-            return res.status(400).json({ error: 'ID, text, scheduled_time, and type are required' });
+            console.error("/modify-message | Body incomplete:", req.body);
+            return res.status(400).json({ error: "/modify-message | Body incomplete: " + req.body });
         }
 
+        // modify the corresponding message according to input values
         const options = {
             text: text,
             scheduled_time: new Date(scheduled_time),
             type: type,
         };
-
         await Message.findOneAndUpdate({ _id: id }, options).then(() => {
-            res.status(201).json({ message: 'Message successfully modified'});
+            console.log("/modify-message | Message", id, "was successfully modified");
+            res.status(201).json({ message: "/modify-message | Message " + id + " was successfully modified" });
         });
 
     } catch (error) {
-        console.error("ERROR | " + error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        console.error("/modify-message |", error);
+        res.status(500).json({ error: "/modify-message | Internal Server Error: " + error });
     }
 })
 
 
-app.put('/clear', async (req, res) => {
+// PRE: -
+// POST: will run through all messages in the database and send those that are scheduled within the next 35 min
+app.post('/check-messages', async (_req, res) => {
     try {
-        await Message.deleteMany({});
-        res.status(201).json({ message: 'All messages cleared'});
-    } catch(error){
-        console.error("ERROR | " + error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-})
-
-
-function timeDif(date1, date2) {
-  // Convert both dates to milliseconds since Jan 1, 1970 00:00:00 UTC
-  const time1 = date1.getTime();
-  const time2 = date2.getTime();
-
-  // Calculate the time difference in milliseconds
-  const timeDifference = Math.abs(time2 - time1);
-
-  // Convert milliseconds to minutes
-  const minutesDifference = Math.floor(timeDifference / (1000 * 60));
-
-  return minutesDifference;
-}
-
-
-app.post('/check-messages', async (req, res) => {
-    try {
-        console.log("Checking messages");
         const now = new Date();
-        const then = new Date(now.getTime() + 30*60000);
+        const then = new Date(now.getTime() + 35*60000);
 
+        // get all messages that are scheduled within the next 35 min
         const messages = await Message.find({
             scheduled_time: {
                 $gte: now,
@@ -160,12 +189,15 @@ app.post('/check-messages', async (req, res) => {
             }
         });
 
+        // have the telegram bot send each message
         messages.forEach(async (entry) => {
-            const remaining_time = timeDif(new Date(), entry.scheduled_time);
+            const remaining_time = helpers.timeDif(new Date(), entry.scheduled_time);
             const text = entry.text.replace("//time//", remaining_time + ' min');
             bot.sendMessage(process.env.CHAT_ID, text, { parse_mode: 'HTML'});
         })
 
+        // delete all messages that should have been sent by now
+        // except for todos
         await Message.deleteMany({
             scheduled_time: {
                 $lte: then,
@@ -174,7 +206,8 @@ app.post('/check-messages', async (req, res) => {
             type: "todo"
         })
 
-        let nextNotifDate = new Date();
+        // reschedule todos
+        let nextNotifDate = now;
         nextNotifDate.setDate(nextNotifDate.getDate() + 3);
 
         await Message.updateMany({
@@ -187,19 +220,11 @@ app.post('/check-messages', async (req, res) => {
             scheduled_time: nextNotifDate, 
         })
 
-        res.status(201).json({ message: 'All messages checked'});
+        console.log("/check-messages | All messages checked");
+        res.status(201).json({ message: "/check-messages | All messages checked" });
 
     } catch (error) {
-        console.error("ERROR | " + error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        console.error("/check-messages | " + error);
+        res.status(500).json({ error: "/check-messages | Internal Server Error: " + error });
     }
 });
-
-
-
-
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log('Listening on port ' + PORT);
-    })
-})
