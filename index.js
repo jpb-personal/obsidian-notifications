16e8e7422c0634642da912c7edb495b9f05b2064
jpb-personal <stellachelsey@gmail.com>
2024-02-10 21:05:20 +0100

    Removes typos

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const Message = require('./models/message.js');
 const bot = new TelegramBot(process.env.BOT_TOKEN);
 
 
-export function timeDif(date1, date2) {
+function timeDif(date1, date2) {
   // Convert both dates to milliseconds since Jan 1, 1970 00:00:00 UTC
   const time1 = date1.getTime();
   const time2 = date2.getTime();
